5c44eafdc692c09d1e3b4a419da5ac96e253a100
Abbas Muhammad <abbas51813@gmail.com>
2023-12-11 18:37:51 +0300

    integrate model.h5 for prediction

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const sharp = require('sharp');
+const tf = require('@tensorflow/tfjs-node');
 const path = require('path');
 
 const app = express();
@@ -14,17 +16,39 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle image prediction
-app.post('/predict', upload.single('image'), (req, res) => {
-  // Process the uploaded image using your model
-  // Replace this with your prediction logic
+// Load the .h5 model
+async function loadModel() {
+  const model = await tf.loadLayersModel('model.h5');
+  return model;
+}
 
-  // Example: Get the filename and send a random prediction
-  if (req.file) {
-    const prediction = Math.random() >= 0.5 ? 'Uninfected' : 'Parasitized'; // Random prediction example
-    res.json({ prediction });
-  } else {
-    res.status(400).send('No image uploaded.');
+// Resize image to 128x128
+async function resizeImage(filePath) {
+  const resizedImageBuffer = await sharp(filePath)
+    .resize(128, 128)
+    .toBuffer();
+  return resizedImageBuffer;
+}
+
+// Handle POST request for image prediction
+app.post('/predict', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('No image uploaded.');
+    }
+
+    // Process the uploaded image using the model
+    const resizedImageBuffer = await resizeImage(req.file.buffer);
+    const model = await loadModel();
+    const imageTensor = tf.node.decodeImage(resizedImageBuffer, 3);
+    const prediction = model.predict(imageTensor);
+    const result = prediction.dataSync();
+
+    // Send the prediction result back to the client
+    res.json({ prediction: result });
+  } catch (error) {
+    console.error('Error predicting image:', error);
+    res.status(500).send('Error predicting image.');
   }
 });
 
